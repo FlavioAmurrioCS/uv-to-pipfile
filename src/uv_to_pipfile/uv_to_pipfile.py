c39a6bc9913b7a4ba071dc5b02257c880ab01c8e
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2025-04-11 17:53:30 -0400

    checkpoint

--- src/uv_to_pipfile/uv_to_pipfile.py
+++ src/uv_to_pipfile/uv_to_pipfile.py
@@ -16,6 +16,7 @@ if os.getenv("GET_VENV") == "1":
     raise SystemExit(0)
 from collections import deque
 from typing import TYPE_CHECKING
+from typing import Iterable
 
 if TYPE_CHECKING:
     from typing import Any
@@ -43,43 +44,35 @@ if TYPE_CHECKING:
     class VirtualSource(TypedDict):
         virtual: str
 
-    class RequiresDist(TypedDict):
+    class Dependency(TypedDict):
         name: str
+        marker: NotRequired[str]
         specifier: NotRequired[str]
-        git: NotRequired[str]
-
-    class RequiresDevDev(TypedDict):
-        name: str
-        specifier: str
-
-    class RequiresDev(TypedDict):
-        dev: list[RequiresDevDev]
+        extras: NotRequired[list[str]]
 
     Metadata = TypedDict(
         "Metadata",
         {
-            "requires-dist": List[RequiresDist],
-            "requires-dev": RequiresDev,
+            "requires-dist": List[Dependency],
+            "requires-dev": NotRequired[dict[str, list[Dependency]]],
         },
     )
 
-    class DevDependenciesDev(TypedDict):
-        name: str
-
     class DevDependencies(TypedDict):
-        dev: list[DevDependenciesDev]
+        dev: list[Dependency]
 
-    class Dependency(TypedDict):
-        name: str
-        marker: NotRequired[str]
-
-    class RegistryPackage(TypedDict):
-        name: str
-        version: str
-        source: RegistrySource
-        dependencies: NotRequired[list[Dependency]]
-        sdist: NotRequired[Sdist]
-        wheels: NotRequired[list[Sdist]]
+    RegistryPackage = TypedDict(
+        "RegistryPackage",
+        {
+            "name": str,
+            "version": str,
+            "source": RegistrySource,
+            "dependencies": NotRequired[list[Dependency]],
+            "sdist": NotRequired[Sdist],
+            "wheels": NotRequired[list[Sdist]],
+            "optional-dependencies": NotRequired[dict[str, list[Dependency]]],
+        },
+    )
 
     class GitPackage(TypedDict):
         name: str
@@ -133,7 +126,9 @@ def registry_package_to_dict(package: RegistryPackage) -> dict[str, Any]:
         hashes.append(package["sdist"]["hash"])
     if "wheels" in package:
         hashes.extend([wheel["hash"] for wheel in package["wheels"]])
+    extras = list(package.get("optional-dependencies", {}).keys())
     return {
+        **({} if not extras else {"extras": extras}),
         "hashes": hashes,
         "version": f"=={package['version']}",
     }
@@ -192,7 +187,8 @@ def parse_args(args: list[str] | None) -> tuple[str, str]:
         raise SystemExit(1)
     return uv_lock, pipfile_lock
 
-def python_version(uv_lock: str ) -> str:
+
+def python_version(uv_lock: str) -> str:
     basedir = os.path.dirname(uv_lock)
     python_version_file = os.path.join(basedir, ".python_version")
     if os.path.exists(python_version_file):
@@ -210,15 +206,10 @@ def python_version(uv_lock: str ) -> str:
     return "3.11"
 
 
-
-def main(args: list[str] | None = None) -> int:  # noqa: C901, PLR0912
-    uv_lock, pipfile_lock = parse_args(args)
-
-    if not os.path.exists(uv_lock):
-        print(f"File {uv_lock} does not exist.")
-        return 1
+def parse_packages(
+    uv_lock: str,
+) -> tuple[dict[str, GitPackage], dict[str, RegistryPackage], VirtualPackage]:
     data: UVLock = load_toml(uv_lock)
-
     git_packages: dict[str, GitPackage] = {}
     registry_packages: dict[str, RegistryPackage] = {}
     virtual_packages: dict[str, VirtualPackage] = {}
@@ -235,54 +226,67 @@ def main(args: list[str] | None = None) -> int:  # noqa: C901, PLR0912
 
     if len(virtual_packages) != 1:
         print(f"Expected exactly one virtual package, got {len(virtual_packages)}")
-        return 1
     virtual_package = virtual_packages.popitem()[1]
+    return git_packages, registry_packages, virtual_package
 
-    registry = {x["source"]["registry"] for x in registry_packages.values()}
-    if len(registry) != 1:
-        print(f"Expected exactly one registry, got {len(registry)}")
 
-    selected_registry = registry.pop()
+def get_sources(registry_packages: Iterable[RegistryPackage]) -> list[dict[str, Any]]:
+    registries = {x["source"]["registry"] for x in registry_packages}
+    return [{"name": registry, "url": registry, "verify_ssl": True} for registry in registries]
+
+
+def main(args: list[str] | None = None) -> int:
+    uv_lock, pipfile_lock = parse_args(args)
+
+    git_packages, registry_packages, virtual_package = parse_packages(uv_lock)
+    sources = get_sources(registry_packages.values())
+
+    if len(sources) != 1:
+        print(f"Expected exactly one registry, got {len(sources)}")
 
     pipfile_lock_data: Final = {
         "_meta": {
             "hash": {"sha256": "UVLOCK"},
             "pipfile-spec": 6,
             "requires": {"python_version": python_version(uv_lock)},
-            "sources": [{"name": "pypi", "url": selected_registry, "verify_ssl": True}],
+            "sources": sources,
         },
         "default": {},
         "develop": {},
     }
 
-    queue: deque[str] = deque()
-    queue.extend([dep["name"] for dep in virtual_package.get("dependencies", [])])
+    queue: deque[Dependency] = deque()
+    queue.extend(virtual_package["metadata"]["requires-dist"])
 
     while queue:
-        dep_name = queue.popleft()
+        dep = queue.popleft()
+        dep_name = dep["name"]
         if dep_name in git_packages:
             package = git_packages[dep_name]
             pipfile_lock_data["default"][dep_name] = git_package_to_dict(package)
-            queue.extend([dep["name"] for dep in package.get("dependencies", [])])
+            queue.extend(package.get("dependencies", []))
         elif dep_name in registry_packages:
             package = registry_packages[dep_name]
             pipfile_lock_data["default"][dep_name] = registry_package_to_dict(package)
-            queue.extend([dep["name"] for dep in package.get("dependencies", [])])
+            queue.extend(package.get("dependencies", []))
+            for pkg in package.get("optional-dependencies", {}).values():
+                queue.extend(pkg)
 
-    queue.extend(
-        [dep["name"] for dep in virtual_package.get("dev-dependencies", {}).get("dev", [])]
-    )
+    queue.extend(virtual_package["metadata"].get("requires-dev", {}).get("dev", []))
 
     while queue:
-        dep_name = queue.popleft()
+        dep = queue.popleft()
+        dep_name = dep["name"]
         if dep_name in git_packages:
             package = git_packages[dep_name]
             pipfile_lock_data["develop"][dep_name] = git_package_to_dict(package)
-            queue.extend([dep["name"] for dep in package.get("dependencies", [])])
+            queue.extend(package.get("dependencies", []))
         elif dep_name in registry_packages:
             package = registry_packages[dep_name]
             pipfile_lock_data["develop"][dep_name] = registry_package_to_dict(package)
-            queue.extend([dep["name"] for dep in package.get("dependencies", [])])
+            queue.extend(package.get("dependencies", []))
+            for pkg in package.get("optional-dependencies", {}).values():
+                queue.extend(pkg)
 
     with open(pipfile_lock, "w") as f:
         import json
